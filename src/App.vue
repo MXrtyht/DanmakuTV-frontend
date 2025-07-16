31f903140f106ef844d10608fab63b719319e0c4
Vincent <20222005026@m.scnu.edu.cn>
2025-07-16 20:51:38 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+  import { ref } from 'vue'
+
+  const count = ref(0)
+
+  function increment(){
+    count.value++
+  }
+</script>
+
+<template>
+  <div>测试</div>
+  <button @click="increment">点击了 {{ count }} 次数</button>
+</template>
+
+<style scoped>
+
+</style>
